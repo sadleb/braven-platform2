d3c01f0bc00c639f1b76e4797a88fe8e42fff757
rshipp <github@rshipp.com>
2019-12-19 14:58:41 -0600

    Fix heroku at last

--- spec/capybara_helper.rb
+++ spec/capybara_helper.rb
@@ -2,22 +2,24 @@ RSpec.configure do |config|
   config.include Capybara::DSL
 end
 
-chrome_bin = ENV.fetch('GOOGLE_CHROME_BIN', nil)
-if chrome_bin
-  Selenium::WebDriver::Chrome.path = chrome_bin
-end
-
 chrome_shim = ENV.fetch('GOOGLE_CHROME_SHIM', nil)
+if chrome_shim
+  Selenium::WebDriver::Chrome.path = chrome_shim
 
-chrome_opts = chrome_shim ? { "chromeOptions" => { "binary" => chrome_shim, "remote-debugging-port" => "9222", "headless": true } } : {}
-puts chrome_opts
+  chrome_opts = chrome_shim ? { "chromeOptions" => { "binary" => chrome_shim, "remote-debugging-port" => "9222", "headless" => true } } : {}
+  puts chrome_opts
 
-Capybara.register_driver :chrome do |app|
-  Capybara::Selenium::Driver.new(
-    app,
-    browser: :chrome,
-    desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(chrome_opts)
-  )
+  Capybara.register_driver :chrome do |app|
+    Capybara::Selenium::Driver.new(
+      app,
+      browser: :chrome,
+      desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(chrome_opts)
+    )
+  end
+
+  Capybara.javascript_driver = :chrome
+else
+  Capybara.javascript_driver = :selenium_chrome_headless
 end
 
-Capybara.javascript_driver = :chrome
+
