3e281d7a6cd6c72ebac81823edafb31b4054ff12
rshipp <github@rshipp.com>
2020-01-23 14:46:13 -0600

    Add honeycomb config
    See https://docs.honeycomb.io/getting-data-in/ruby/beeline/#rails

--- config/initializers/honeycomb.rb
+++ config/initializers/honeycomb.rb
@@ -6,6 +6,16 @@ if Rails.application.secrets.honeycomb_write_key && Rails.application.secrets.ho
     Honeycomb.configure do |config|
       config.write_key = Rails.application.secrets.honeycomb_write_key
       config.dataset = Rails.application.secrets.honeycomb_dataset
+      config.notification_events = %w[
+        sql.active_record
+        render_template.action_view
+        render_partial.action_view
+        render_collection.action_view
+        process_action.action_controller
+        send_file.action_controller
+        send_data.action_controller
+        deliver.action_mailer
+      ].freeze
     end
   else
     Rails.logger.warn "Honeycomb Beeline is not install. Skipping initialization."
