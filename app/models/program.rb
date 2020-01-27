6ddd91193cabbffe0177aa6382d852ede10ff425
Brian Sadler <sadler.brian@gmail.com>
2020-01-27 14:47:24 -0500

    Add note about the "program" model to keep in mind as we model out all the rostering stuff.

--- app/models/program.rb
+++ app/models/program.rb
@@ -3,6 +3,18 @@ class Program < ApplicationRecord
   has_many :people, through: :program_memberships
   has_many :roles, through: :program_memberships
 
+
+  # PROPOSAL: as we model this out, here is goal I want to propose:
+  # Divorce the logic of how a "course" is laid out including what content, projects, lessons
+  # grading rules, etc from the logic needed to actually execute a course and the logistics 
+  # inolved in running it. E.g. the people in it, their role, the due dates, the submissions,
+  # etc. 
+  # 
+  # We shouldn't have to copy everything over and over again when it's the same "course."
+  # A "program" should just point to a course and have all the administration / logistics
+  # tied to the program. So a program is an "instance" of a course that we are executing for
+  # a given semester at a given school. 
+
   validates :name, presence: true, uniqueness: {case_sensitive: false}
   
   def to_show
