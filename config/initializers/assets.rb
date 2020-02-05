93f553daa55c8691b6b72beb001ade4d73efc3f0
Brian Rose <stetsentech@gmail.com>
2020-02-05 09:01:52 -0500

    Adds fonts to precompile

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,3 +13,7 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 Rails.application.config.assets.precompile += %w( cas/login.css )
+
+# Add fonts to precompile
+Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
+
