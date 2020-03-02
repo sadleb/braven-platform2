8d1d5f28044f61ca6d4f4a9153085ef9a704ea1a
rshipp <github@rshipp.com>
2020-03-02 12:50:14 -0600

    Pluralize has_many

--- app/models/course_content.rb
+++ app/models/course_content.rb
@@ -1,6 +1,6 @@
 class CourseContent < ApplicationRecord
-  has_many :course_content_history
-  has_many :course_content_undo
+  has_many :course_content_histories
+  has_many :course_content_undos
 
   def publish(params)
     response = CanvasProdClient.update_course_page(params[:course_id], params[:secondary_id], params[:body])
