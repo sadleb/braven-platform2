f7778eb7673418033785d1559a50438e795be465
rshipp <github@rshipp.com>
2020-03-02 13:03:18 -0600

    Clean up schema

--- db/schema.rb
+++ db/schema.rb
@@ -34,15 +34,6 @@ ActiveRecord::Schema.define(version: 2020_02_20_212829) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "contact_owners", force: :cascade do |t|
-    t.integer "contact_id"
-    t.string "contact_type"
-    t.integer "owner_id"
-    t.string "owner_type"
-    t.index ["contact_id", "contact_type"], name: "index_contact_owners_on_contact_id_and_contact_type"
-    t.index ["owner_id", "owner_type"], name: "index_contact_owners_on_owner_id_and_owner_type"
-  end
-
   create_table "course_content_histories", force: :cascade do |t|
     t.bigint "course_content_id", null: false
     t.string "title"
@@ -50,17 +41,6 @@ ActiveRecord::Schema.define(version: 2020_02_20_212829) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["course_content_id"], name: "index_course_content_histories_on_course_content_id"
-  end
-
-  create_table "course_content_undos", force: :cascade do |t|
-    t.bigint "course_content_id", null: false
-    t.text "operation"
-    t.integer "version"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.integer "batch_version"
-    t.index ["course_content_id", "version"], name: "index_course_content_undos_on_course_content_id_and_version", unique: true
-    t.index ["course_content_id"], name: "index_course_content_undos_on_course_content_id"
   end
 
   create_table "course_contents", force: :cascade do |t|
@@ -374,6 +354,7 @@ ActiveRecord::Schema.define(version: 2020_02_20_212829) do
     t.index ["email"], name: "index_users_on_email"
   end
 
+  add_foreign_key "course_content_histories", "course_contents"
   add_foreign_key "grade_categories", "programs"
   add_foreign_key "lesson_submissions", "lessons"
   add_foreign_key "lesson_submissions", "users"
@@ -395,6 +376,4 @@ ActiveRecord::Schema.define(version: 2020_02_20_212829) do
   add_foreign_key "sections", "programs"
   add_foreign_key "user_sections", "sections"
   add_foreign_key "user_sections", "users"
-  add_foreign_key "course_content_histories", "course_contents"
-  add_foreign_key "course_content_undos", "course_contents"
 end
