0e13b4369e0b12bfa145c8c2f7d9a6841a483977
rshipp <github@rshipp.com>
2020-04-02 14:00:22 -0500

    Fix table cell handling, update comments

--- app/javascript/ckeditor/customelementattributepreservation.js
+++ app/javascript/ckeditor/customelementattributepreservation.js
@@ -174,9 +174,10 @@ function setupAllowedAttributePreservation( editor ) {
     // Elements we want to allow custom attribute preservation on.
     // { view: model }
     // Note: hN = heading(N-1) is a ckeditor5 convention.
+    // We can do all these with normal priority because the ones we define in ContentEditor.js
+    // in the CKE heading options are set to `low` priority.
     const elements = {
         'p': 'paragraph',
-        'td': 'tableCell',
         'h2': 'heading1',
         'h3': 'heading2',
         'h4': 'heading3',
@@ -194,8 +195,17 @@ function setupAllowedAttributePreservation( editor ) {
         } );
     } );
 
+    // TD converter must be high priority so it overrides the CKE builtin tableCell converters.
+    editor.conversion.for( 'upcast' ).elementToElement( {
+        view: 'td',
+        model: ( viewElement, modelWriter ) => {
+            return modelWriter.createElement( 'tableCell', filterAllowedAttributes( viewElement.getAttributes() ) );
+        },
+        // Use low priority to make sure existing converters run first.
+        converterPriority: 'high'
+    } );
 
-    // Div and span converters must be regular priority so they don't override more specific converters defined elsewhere.
+    // Div and span converters must be normal priority or lower so they don't override more specific converters defined elsewhere.
     editor.conversion.for( 'upcast' ).elementToElement( {
         view: 'div',
         model: ( viewElement, modelWriter ) => {
