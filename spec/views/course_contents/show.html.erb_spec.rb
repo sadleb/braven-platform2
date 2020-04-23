658ac39b217051b9db7442675ce6d830009a9fef
rshipp <github@rshipp.com>
2020-04-23 13:26:36 -0500

    Break test in two

--- spec/views/course_contents/show.html.erb_spec.rb
+++ spec/views/course_contents/show.html.erb_spec.rb
@@ -14,12 +14,13 @@ RSpec.describe "course_contents/show", type: :view do
     expect(rendered).to match(/<p>MyText<\/p>/)
   end
 
-  it "adds appropriate div class for content_type" do
-    # module first
+  it "adds appropriate div class for module" do
     render
     expect(rendered).to match(/<div class="bz-module">/)
+  end
 
-    # then assignment
+
+  it "adds appropriate div class for assignment" do
     assign(:course_content, CourseContent.create!(
       :title => "Title",
       :body => "<p>MyText</p>",
