0481397621db77b05a5da801d2ae1bb60ae2a083
rshipp <github@rshipp.com>
2020-05-05 17:43:19 -0500

    Make docker faster

--- docker-compose/scripts/docker_compose_run.sh
+++ docker-compose/scripts/docker_compose_run.sh
@@ -35,6 +35,13 @@ else
     cp_built_lock_file
 fi
 
+
+# Fix "inotify event queue has overflowed."
+# https://github.com/guard/listen/wiki/Increasing-the-amount-of-inotify-watchers
+sysctl fs.inotify.max_user_watches=524288
+sysctl fs.inotify.max_queued_events=524288
+sysctl fs.inotify.max_user_instances=524288
+
 # Note: there are some issues with the listen gem and certain editors
 # where gaurd won't detect changes made from the host machine on a Mac
 # inside the container when the volume is mounted. For VIM, you need to add
