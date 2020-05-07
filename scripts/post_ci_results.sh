c079322fb03a84237d567ef6cf98b325550132c2
Brian Sadler <sadler.brian@gmail.com>
2020-05-07 17:01:25 -0400

    Alpine linux vs bash bites me again.

--- scripts/post_ci_results.sh
+++ scripts/post_ci_results.sh
@@ -1,1 +1,5 @@
-wget -qO - https://coverage.codacy.com/get.sh | sh -s report -r coverage/coverage.xml
+# Alpine linux command:
+#wget -qO - https://coverage.codacy.com/get.sh | sh -s report -r coverage/coverage.xml
+
+# Bash
+bash <(curl -Ls https://coverage.codacy.com/get.sh) report -r coverage/coverage.xml
