273b3bbe716d7a45b75b3d05cde0d238d7923324
Brian Sadler <sadler.brian@gmail.com>
2020-05-09 12:42:33 -0400

    Refactor javascripts to match the new layouts.
    Added content_editor and accounts layouts. This commit
    refactors the javascripts for those layouts to use properly.
    
    Note: Rails 6 moved from the Sprockets assets pipeline to a Webpack
    pipeline for javascripts. This removes the old Sprokects javascripts files
    which weren't used. See: https://prathamesh.tech/2019/08/26/understanding-webpacker-in-rails-6/
    
    Test Plan:
    - load the sign-in and sign-up pages and make sure they link in the correct javascripts for that layout.
    - same for the content_editor.

---
+++ app/javascript/packs/accounts.js
@@ -0,0 +1,5 @@
+// This is the main entrypoint for the accounts layout
+// Things that need to be set up globally for all accounts related pages go here.
+// Run this script by adding <%= javascript_pack_tag 'accounts' %> to the head of your layout file,
+// like app/views/layouts/accounts.html.erb.
+
--- app/javascript/packs/sidebar.js
+++ app/javascript/packs/sidebar.js
@@ -1,7 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+// This is the main entrypoint for the Sidebar.
+// Things that need to be set up globally for the sidebar go here.
+// Run this script by adding <%= javascript_pack_tag 'sidebar' %> to the head of your layout file,
+// like app/views/layouts/content_editor.html.erb.
+
+
+// Dev Only: makes guard-webpacker and hot reloading work.
 import WebpackerReact from 'webpacker-react'
-
 import Sidebar from 'components/Sidebar'
-
 WebpackerReact.setup({Sidebar})
