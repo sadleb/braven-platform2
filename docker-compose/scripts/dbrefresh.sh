187a01777e915f9dac8aec83007bcabc6160e590
Brian Sadler <sadler.brian@gmail.com>
2020-06-24 08:44:34 -0400

    Fix dbrefresh script.
    The web container fails to come up without the database, so create the database
    first and then bring it back up. Note: that `docker-compose exec` connects to the
    running container and `docker-compose run` spins up a new container with the
    given command.

--- docker-compose/scripts/dbrefresh.sh
+++ docker-compose/scripts/dbrefresh.sh
@@ -3,15 +3,13 @@ echo "Refreshing your local dev database"
 
 docker-compose down
 docker volume rm platform_db-platform
+docker-compose run platformweb bundle exec rake db:create db:migrate db:dummies
 docker-compose up -d
-sleep 5 # wait for containers to be up and accepting connections
-
-docker-compose exec platformweb bundle exec rake db:create db:migrate db:dummies
 
 # Note: this is fast than a db:migrate but if your'e actively working on db migrations
 # you're likely doing a dbrefresh to get back to a pristine state after you've been
 # iterating and db/schema.rb may not be correct yet.
-#docker-compose exec platformweb bundle exec rake db:create db:schema:load db:dummies
+#docker-compose run platformweb bundle exec rake db:create db:schema:load db:dummies
 
 
 
