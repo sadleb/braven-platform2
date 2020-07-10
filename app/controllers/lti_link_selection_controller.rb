cd60cc0daad72000df6a7db89bb65374a97d6bd4
Jess Ouyang <jess.ouyang@bebraven.org>
2020-07-10 12:57:05 -0700

    New endpoints, things working with LTI assignment selection push

--- app/controllers/lti_link_selection_controller.rb
+++ app/controllers/lti_link_selection_controller.rb
@@ -3,13 +3,17 @@ class LtiLinkSelectionController < ApplicationController
   skip_before_action :ensure_admin!
   skip_before_action :verify_authenticity_token
   
-  def index
+  # Non-standard controller without normal CRUD methods. Disable the convenience module.
+  def dry_crud_enabled?() false end
+
+  def new
   	# There's a way to configure this. See: 
   	# https://stackoverflow.com/questions/18445782/how-to-override-x-frame-options-for-a-controller-or-action-in-rails-4
-  	response.headers["X-FRAME-OPTIONS"] = "ALLOW-FROM https://braven.instructure.com"
+  	# response.headers["X-FRAME-OPTIONS"] = "ALLOW-FROM https://braven.instructure.com"
 
   	@canvas_user_id = "1234test!"
 
+  	# TODO: Access these from the shell environment instead
   	# Connect to S3 using .env configuration
   	credentials = Aws::Credentials.new(ENV["AWS_ACCESS_KEY_ID"], ENV["AWS_SECRET_ACCESS_KEY"])
   	Aws.config.update({credentials: credentials})
@@ -22,20 +26,20 @@ class LtiLinkSelectionController < ApplicationController
   	# obj.upload_file('unlock-your-hustle.zip')
 
   	# Now, try unzipping it locally
-	Zip::File.open('unlock-your-hustle.zip') do |zip_file|
-  	  zip_file.each do |file|
-  	  	puts "Extracting #{file.name}"
-  	  	filepath = File.join('tmp/unlock-your-hustle/', file.name)
-  	  	FileUtils.mkdir_p(File.dirname(filepath))
-  	  	zip_file.extract(file, filepath)
-    	# obj = bucket.object('unlock-your-hustle-test/' + entry.name)
-    	# obj.upload_file(entry.name)
-  		end
-  	end
+	# Zip::File.open('unlock-your-hustle.zip') do |zip_file|
+ #  	  zip_file.each do |file|
+ #  	  	puts "Extracting #{file.name}"
+ #  	  	filepath = File.join('tmp/unlock-your-hustle/', file.name)
+ #  	  	FileUtils.mkdir_p(File.dirname(filepath))
+ #  	  	zip_file.extract(file, filepath)
+ #    	# obj = bucket.object('unlock-your-hustle-test/' + entry.name)
+ #    	# obj.upload_file(entry.name)
+ #  		end
+ #  	end
   	# Upload recursively
   	# Delete all the local stuff
   	# Print the link
-
+ 
   end
 
   def create
