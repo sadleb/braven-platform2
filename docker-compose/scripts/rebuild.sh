7c130a1b143d9cee19b362f6b2af9ecc53601a41
Brian Sadler <sadler.brian@gmail.com>
2020-07-11 10:36:41 -0400

    Add note about going nucular (sic) when rebuilding docker

--- docker-compose/scripts/rebuild.sh
+++ docker-compose/scripts/rebuild.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 docker-compose down
 docker volume rm platform_vendor-bundle
+# If you have to go nuclear, run:
+# docker-compose build --no-cache && docker-compose up -d
 docker-compose up -d --force-recreate --build
