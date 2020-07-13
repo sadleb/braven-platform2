dd44b98213019c081fe2f1ad76b20d41fcf84ce0
Abdulmajid Hamza <abdulmajid.hamza@gmail.com>
2020-07-13 16:39:54 +0100

    Add mailer spec

---
+++ spec/mailers/salesforce_to_lms_sync_mailer_spec.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe SalesforceToLmsSyncMailer, type: :mailer do
+  describe '#success_email' do
+    let(:recipient) { 'example@example.com' }
+    let(:mail) { SalesforceToLmsSyncMailer.with(email: recipient).success_email }
+
+    # before(:each) { mail.deliver_now }
+
+    it 'has the right subject' do
+      expect(mail.subject).to eql('Sync Successful')
+    end
+
+    it 'has the right recipient' do
+      expect(mail.to).to eql([recipient])
+    end
+
+    it 'renders the right body' do
+      expect(mail.body.encoded).to match('successful')
+    end
+  end
+
+  describe '#failure_email' do
+    let(:recipient) { 'example@example.com' }
+    let(:mail) { SalesforceToLmsSyncMailer.with(email: recipient).failure_email }
+
+    # before(:each) { mail.deliver_now }
+
+    it 'has the right subject' do
+      expect(mail.subject).to eql('Sync Failed')
+    end
+
+    it 'has the right recipient' do
+      expect(mail.to).to eql([recipient])
+    end
+
+    it 'renders the right body' do
+      expect(mail.body.encoded).to match('failed')
+    end
+  end
+
+end
