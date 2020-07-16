9bc0e95fcf34e9ddfbe753d05e54efc97a97d940
Jess Ouyang <jess.ouyang@bebraven.org>
2020-07-16 11:08:08 -0700

    Remove LtiLinkSelection stuff

--- app/controllers/lti_link_selection_controller.rb
+++
@@ -1,4 +0,0 @@
-class LtiLinkSelectionController < ApplicationController
-  # Non-standard controller without normal CRUD methods. Disable the convenience module.
-  def dry_crud_enabled?() false end
-end
