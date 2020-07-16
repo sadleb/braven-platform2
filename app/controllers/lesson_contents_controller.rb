aef75ac9dd75a99a35e63d04dcc7819275d521af
Jess Ouyang <jess.ouyang@bebraven.org>
2020-07-16 11:16:25 -0700

    Fixes from Brian's comments

--- app/controllers/lesson_contents_controller.rb
+++ app/controllers/lesson_contents_controller.rb
@@ -7,7 +7,6 @@ class LessonContentsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def new
-    @lesson_content = LessonContent.new
   end
 
   def create
@@ -18,21 +17,18 @@ class LessonContentsController < ApplicationController
     lti_launch = LtiLaunch.current(params[:state])
 
     @lesson_content = LessonContent.new(lesson_content_create_params)
-    @lesson_content.save
+    @lesson_content.save!
 
     # TODO: Extract this asynchronously
     # https://app.asana.com/0/1174274412967132/1184800386160057
-    @s3_index_url = @lesson_content.extract
+    @lesson_content.extract
     lesson_url = lesson_content_url(@lesson_content)
 
-    # TODO: Decide whether it's better to use lesson_url that does a redirect
-    # https://app.asana.com/0/1174274412967132/1184800386160059
     @deep_link_return_url, @jwt_response = helpers.lti_deep_link_response_message(lti_launch, lesson_url)
   end
 
   def show
-    lesson_content = LessonContent.find(params[:id])
-    redirect_to lesson_content.get_index_url
+    redirect_to @lesson_content.get_index_url
   end
 
   private
