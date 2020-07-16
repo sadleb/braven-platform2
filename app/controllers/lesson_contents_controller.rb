019c16835a7316fec9558314bcecd8d5c1d15d9e
Jess Ouyang <jess.ouyang@bebraven.org>
2020-07-15 18:30:16 -0700

    Clean up, verify it works end-to-end

--- app/controllers/lesson_contents_controller.rb
+++ app/controllers/lesson_contents_controller.rb
@@ -1,6 +1,11 @@
 require 'zip'
 
 class LessonContentsController < ApplicationController
+	# TODO: Get rid of this, something weird is going on right now
+	skip_before_action :authenticate_user!
+	skip_before_action :ensure_admin!
+	skip_before_action :verify_authenticity_token
+
 	def new
 		@lesson_content = LessonContent.new
 	end
@@ -12,73 +17,22 @@ class LessonContentsController < ApplicationController
 
 		@lesson_content = LessonContent.new(lesson_content_params)
 		@lesson_content.save
-
+		
+		# TODO: Decide whether it's better to use lesson_url that does the 
+		# redirect to S3, or the S3 url directly, for some reason it doesn't
+		# want to redirect through S3? 
+		@s3_index_url = @lesson_content.extract
 		lesson_url = lesson_content_url(@lesson_content)
     	@deep_link_return_url, @jwt_response = helpers.lti_deep_link_response_message(lti_launch, lesson_url)
 	end
 
 	def show
 		lesson_content = LessonContent.find(params[:id])
-		@index = lesson_content.get_index_url
-	end
-
-	def unzip_upload
-
-		@path = url_for(lesson_content.lesson_content_zipfile)
-
-		@bucket_prefix = 'lessons/' + lesson_content.lesson_content_zipfile.key + '/'
-
-		# Connect to AWS S3
-	    credentials = Aws::Credentials.new(ENV["AWS_ACCESS_KEY_ID"], ENV["AWS_SECRET_ACCESS_KEY"])
-	    Aws.config.update({credentials: credentials})
-	    s3 = Aws::S3::Resource.new(region: ENV["AWS_REGION"])
-	    bucket = s3.bucket(ENV["AWS_S3_BUCKET"])
-
-	    # Create a client so we can set permissions
-	    client = Aws::S3::Client.new(region: ENV["AWS_REGION"])
-
-		# Unzip
-		# TODO: alternate: store .zip on disk with ActiveStorage 
-		# since we don't really need to persist it instead of downloading
-		@all_files = ''
-		@all_dirs = ''
-		lesson_content.lesson_content_zipfile.open do |file|
-			@filepath = file.path
-			Zip::File.open(file.path) do |zip_file|
-				zip_file.each do |entry|
-					if entry.file?
-						@all_files +=  "#{entry.name}"
-
-						# Upload the temporary file to the bucket
-						# This gets us the right directory structure
-						object_key = @bucket_prefix + "#{entry.name}"
-
-						s3_obj = bucket.object(object_key)
-						s3_obj.put(body: entry.get_input_stream.read)
-
-						# TODO: Make each object public read as we're making it?
-						client.put_object_acl( {
-							acl: "public-read",
-							bucket: ENV["AWS_S3_BUCKET"],
-							key: object_key,
-						})
-					elsif entry.directory? 
-						@all_dirs += "#{entry.name}"
-					end
-				end
-			end
-
-			# Now that we've succeeded, make the bucket public
-	    	client.put_bucket_acl({
-  				acl: "public-read",
-  				bucket: ENV["AWS_S3_BUCKET"],
-			})
-		end
+		redirect_to lesson_content.get_index_url
 	end
 
 	private
     def lesson_content_params
     	params.permit(:lesson_content_zipfile)
     end
-
 end
--- app/models/lesson_content.rb
+++ app/models/lesson_content.rb
@@ -8,16 +8,44 @@ class LessonContent < ApplicationRecord
 
   # Publicly accessible URL for the lesson
   def get_index_url
-    s3_bucket.object(s3_object_key(INDEX_FILE)).public_url
+    bucket.object(s3_object_key(INDEX_FILE)).public_url
   end
 
-  # For a file in the zip, returns an S3 object key for a file in the zip
+  # Return an S3 object key for a file in the zip
+  # for a relative filepath in lesson_content_zipfile
   def s3_object_key(filename)
     [ S3_OBJECT_PREFIX, self.lesson_content_zipfile.key, filename ].join("/")
   end
 
+  # Extract the lesson_content_zipfile and store it on S3
+  # Return index to access content
+  def extract
+    return get_index_url unless !bucket.object(s3_object_key(INDEX_FILE)).exists?
+
+    # Extract the zipfile
+    credentials = Aws::Credentials.new(ENV["AWS_ACCESS_KEY_ID"], ENV["AWS_SECRET_ACCESS_KEY"])
+    Aws.config.update({credentials: credentials})
+    bucket = Aws::S3::Resource.new(region: ENV["AWS_REGION"]).bucket(ENV["AWS_S3_BUCKET"])
+
+    self.lesson_content_zipfile.open do |file|
+      Zip::File.open(file.path) do |zip_file|
+        zip_file.each do |entry|
+          next unless entry.file?
+            s3_object = bucket.object(s3_object_key(entry.name))
+            s3_object.put({
+              acl: "public-read",
+              body: entry.get_input_stream.read,
+            })
+          end
+        end
+      end
+
+    get_index_url
+  end
+
   # Return S3 bucket
-  def s3_bucket
+  private
+  def bucket
     credentials = Aws::Credentials.new(ENV["AWS_ACCESS_KEY_ID"], ENV["AWS_SECRET_ACCESS_KEY"])
     Aws.config.update({credentials: credentials})
     s3 = Aws::S3::Resource.new(region: ENV["AWS_REGION"])
