91b01bb8e1735cf4ca35934eaa91aed828786390
Jess Ouyang <jess.ouyang@bebraven.org>
2020-07-20 17:47:14 -0700

    Basic GET test runs
    Need to ./dbrefresh between runs because user is already created?
    Seems like some DB isn't being cleared out between runs, or the user object
    is being added to some untracked DB?
    See FIXME.

--- spec/controllers/lesson_contents_controller.spec.rb
+++ spec/controllers/lesson_contents_controller.spec.rb
@@ -1,10 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe LessonContentsController, type: :controller do
-
-  # TODO: once we fix this up to not require a logged in platform user, remove this.
-  # https://app.asana.com/0/1174274412967132/1184057808812010
+  # TODO: https://app.asana.com/0/1174274412967132/1184057808812010
   render_views
+
+  # FIXME: Getting an error that the user already exists, seems like somehow the DB isn't being cleared
+  # out: https://github.com/thoughtbot/factory_bot/issues/1158,
+  # but I can't find anywhere where we define user/admin_user outside of factories
+  # I have to dbrefresh between runs :(
   let(:user) { create :admin_user }
 
   before(:each) do
@@ -19,42 +22,15 @@ RSpec.describe LessonContentsController, type: :controller do
   let(:lti_launch) { create(:lti_launch_deep_link, target_link_uri: target_link_uri, state: state) }
   let!(:lesson_content) { create(:lesson_content) }
 
-  # describe "GET #new" do
-  #   it "returns a success response" do
-  #     get :new, params: {state: state}, session: valid_session
-  #     expect(response).to be_successful
-  #   end
+  describe "GET #new" do
+    it "returns a success response" do
+      get :new, params: {state: state}, session: valid_session
+      expect(response).to be_successful
+    end
 
-  #   it "includes includes a file uploader" do
-  #     get :new, params: {state: state}, session: valid_session
-  #     expect(response.body).to match /<input type="file" name="lesson_content_zipfile" id="lesson_content_zipfile"/
-  #   end
-  # end
-
-  # describe "POST #create" do
-  #   context "with valid params" do
-  #     it "shows the confirmation form and preview iframe" do
-  #       expected_url = LtiDeepLinkingRequestMessage.new(lti_launch.id_token_payload).deep_link_return_url
-
-  #       post :create, params: {state: state, assignment_id: assignment.id}, session: valid_session
-  #       expect(response.body).to match /<form action="#{expected_url}"/
-  #       expect(response.body).to match /<iframe src=".*\/#{assignment.id}"/
-  #     end
-  #   end
-
-  #   context "with invalid params" do
-  #     it "raises an error when state param is missing" do
-  #       expect {
-  #         post :create, params: {assignment_id: assignment.id}, session: valid_session
-  #       }.to raise_error ActionController::ParameterMissing
-  #     end
-
-  #     it "raises an error when assignment_id param is missing" do
-  #       expect {
-  #         post :create, params: {state: state}, session: valid_session
-  #       }.to raise_error ActionController::ParameterMissing
-  #     end
-  #   end
-  # end
-
+    it "includes a file input" do
+      get :new, params: {state: state}, session: valid_session
+      expect(response.body).to match /<input type="file" name="lesson_content_zipfile" id="lesson_content_zipfile"/
+    end
+  end
 end
