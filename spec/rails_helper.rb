dfe19bcaed252859c40d359ed465110a5ab7dbaf
Jess Ouyang <jess.ouyang@bebraven.org>
2020-07-20 17:38:33 -0700

    Update configurations to get things working with ActiveStorage, S3 endpoint requests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -56,10 +56,8 @@ RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
-  config.use_transactional_fixtures = true
+  # We use ActiveRecord, so this needs to be set to false
+  config.use_transactional_fixtures = false
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
@@ -89,7 +87,6 @@ RSpec.configure do |config|
     # Mimic the router behavior of setting the Devise scope through the env.
     @request.env['devise.mapping'] = Devise.mappings[:user]
   end
- 
 end
 
 Shoulda::Matchers.configure do |config|
