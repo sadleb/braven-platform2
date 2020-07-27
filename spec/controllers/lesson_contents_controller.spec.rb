e21c5756d374b55937ad6affc24b71aeda5d9ff9
jessouyangbraven <62911934+jessouyangbraven@users.noreply.github.com>
2020-07-27 14:23:39 -0700

    VCR record and fix RSpec test for LessonContentsController (#289)
    https://github.com/bebraven/platform/pull/289

--- spec/controllers/lesson_contents_controller.spec.rb
+++ spec/controllers/lesson_contents_controller.spec.rb
@@ -13,13 +13,7 @@ RSpec.describe LessonContentsController, type: :controller do
 
   # TODO: https://app.asana.com/0/1174274412967132/1184057808812010
   let(:valid_session) { {} } 
-
   let(:state) { SecureRandom.uuid }
-  let(:target_link_uri) { 'https://target/link' }
-  let!(:lti_launch) { create(:lti_launch_deep_link, target_link_uri: target_link_uri, state: state) }
-  let(:lesson_content) { create(:lesson_content) }
-  let(:lesson_content_with_zipfile) { create(:lesson_content_with_zipfile) }
-  let(:lesson_content_zipfile) { fixture_file_upload(Rails.root.join('spec/fixtures', 'example_rise360_package.zip'), 'application/zip') }
 
   describe "GET #new" do
     it "returns a success response" do
@@ -34,6 +28,7 @@ RSpec.describe LessonContentsController, type: :controller do
   end
 
   describe "GET #show" do
+    let(:lesson_content_with_zipfile) { create(:lesson_content_with_zipfile) }
     context 'existing lesson content' do
       it 'redirects to public url' do
         launch_url = 'https://S3-bucket-path/lessons/somekey/index.html'
@@ -46,10 +41,13 @@ RSpec.describe LessonContentsController, type: :controller do
   end
 
   describe "POST #create" do
+    let(:file_upload) { fixture_file_upload(Rails.root.join('spec/fixtures', 'example_rise360_package.zip'), 'application/zip') }
+    let!(:lti_launch) { create(:lti_launch_deep_link, target_link_uri: 'https://target/link', state: state) }
+
     context "with invalid params" do
       it "raises an error when state param is missing" do
         expect {
-          post :create, params: {lesson_content_zipfile: lesson_content_zipfile}, session: valid_session
+          post :create, params: {lesson_content_zipfile: file_upload}, session: valid_session
         }.to raise_error ActionController::ParameterMissing
       end
 
@@ -63,21 +61,25 @@ RSpec.describe LessonContentsController, type: :controller do
     context "with valid params" do
       it "shows the confirmation form and preview iframe" do
         expected_url = LtiDeepLinkingRequestMessage.new(lti_launch.id_token_payload).deep_link_return_url
-        post :create, params: {state: state, lesson_content_zipfile: lesson_content_zipfile}, session: valid_session
+        # The zipfile key changes and is par tof the request, only match on host
+        VCR.use_cassette('lesson_content_zipfile_upload', :match_requests_on => [:host]) do
+          post :create, params: {state: state, lesson_content_zipfile: file_upload}, session: valid_session
+        end
         expect(response.body).to match /<form action="#{expected_url}"/
 
         lesson_content_url = lesson_content_url(LessonContent.last)
         expect(response.body).to match /<iframe id="lesson-content-preview" src="#{lesson_content_url}"/
       end
 
-      # From: https://www.dwightwatson.com/posts/testing-activestorage-uploads-in-rails-52
       it 'attaches uploaded zipfile' do
-        expect {
-          post :create, params: {state: state, lesson_content_zipfile: lesson_content_zipfile} , session: valid_session
-        }.to change(ActiveStorage::Attachment, :count).by(1)
-        expect(LessonContent.last.lesson_content_zipfile).to be_attached
+        # The zipfile key changes and is par tof the request, only match on host
+        VCR.use_cassette('lesson_content_zipfile_upload', :match_requests_on => [:host]) do
+          expect {
+            post :create, params: {state: state, lesson_content_zipfile: file_upload} , session: valid_session
+          }.to change(ActiveStorage::Attachment, :count).by(1)
+          expect(LessonContent.last.lesson_content_zipfile).to be_attached
+        end
       end
     end
   end
-
 end
