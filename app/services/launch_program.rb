4aba70f07ab304ec9953b55436c630f62d54f0ab
Brian Sadler <sadler.brian@gmail.com>
2020-10-21 13:20:29 -0400

    Setup Canvas Sections in newly launched Canvas courses (#380)
    * Setup Canvas Sections in newly launched Canvas courses
    
    When the New Program Launch page is used to launch a new program
    and create Accelerator and LC Playbooks from their respective templates,
    this commit adds functionality to setup the initial list of Canvas
    Sections in those courses and associate them with every assignment
    so that a staff member can use the Edit Assignment Dates UI to set the
    due dates for each assignment on a section by section basis.
    
    Test Plan:
    - Create two course templates your local database with real Canvas course
      IDs. E.g.
      `CourseTemplate.create! name: 'Accelerator Template', canvas_course_id: 48`
      `CourseTemplate.create! name: 'LC Playbook Template', canvas_course_id: 185`
    - Setup 2 Cohort Schedules on the Salesforce Program object you're going to launch.
    - Set the "Section Name in LMS Coach Course" field on that Salesforce Program.
    - Use the "New Program Launch" page here: https://platformweb/course_management/launch
      to launch that program using your two templates.
    - You should get a success email (or have it show in the server logs) after a minute or
      two and if you go into Canvas, both courses should be there with sections showing for every
      assignment on the Edit Assignment Dates page from the Assignments tab.
      Note: the Accelerator will have a section per Cohort Schedule and the LC Playbook will
      just have a catch-all section taken from the "Section Name in LMS Coach Course" field
    
    * Fix broken specs and add Program validation ones.
    
    * Implement specs for adding sections during New Program Launch.
    
    * Refactor app/services/launch_program.rb
    
    Make it cleaner and remove my brainstorming comments.
    
    * Rename LaunchProgram.setup_canvas_sections
    
    create_canvas_sections() is clearer b/c setup is a noun. Code review feedback.

--- app/services/launch_program.rb
+++ app/services/launch_program.rb
@@ -1,23 +1,36 @@
 # frozen_string_literal: true
 
 class LaunchProgram
+  LaunchProgramError = Class.new(StandardError)
+
   def initialize(salesforce_program_id, fellow_course_template_id, fellow_course_name, lc_course_template_id, lc_course_name)
-    @salfesforce_program_id = salesforce_program_id
+
+    set_salesforce_vars(salesforce_program_id)
+
     @fellow_course_template = CourseTemplate.find(fellow_course_template_id)
     @fellow_course = Course.create!(
       name: fellow_course_name,
       course_resource: @fellow_course_template.course_resource
     )
+
     @lc_course_template = CourseTemplate.find(lc_course_template_id)
     @lc_course = Course.create!(
       name: lc_course_name,
     )
   end
 
-  # Raise exceptions from here so they can be handled in the controller.
   def run
-    LaunchProgram.canvas_launch!(@fellow_course, @fellow_course_template)
-    LaunchProgram.canvas_launch!(@lc_course, @lc_course_template)
+    # Kick off both launches before we start waiting on them.
+    fellow_launch_response = LaunchProgram.canvas_launch!(@fellow_course, @fellow_course_template)
+    lc_launch_response = LaunchProgram.canvas_launch!(@lc_course, @lc_course_template)
+
+    LaunchProgram.after_canvas_launch_completes!(fellow_launch_response) do
+      LaunchProgram.create_canvas_sections!(@fellow_course, @fellow_course_section_names)
+    end
+
+    LaunchProgram.after_canvas_launch_completes!(lc_launch_response) do
+      LaunchProgram.create_canvas_sections!(@lc_course, @lc_course_section_names)
+    end
   end
 
   def self.canvas_launch!(course, course_template)
@@ -27,4 +40,67 @@ class LaunchProgram
     course.update!(canvas_course_id: canvas_course_data['id'])
     CanvasAPI.client.copy_course(course_template.canvas_course_id, course.canvas_course_id)
   end
+
+  def self.after_canvas_launch_completes!(launch_response, &block)
+    response = launch_response
+    progress_url = response['progress_url']
+    start_time = Time.now
+    while (response['workflow_state'] != 'completed' && response['workflow_state'] != 'failed')
+      raise LaunchProgramError, "Canvas course copy failed. See: #{response['migration_issues_url']}" if response['workflow_state'] == 'failed'
+      raise LaunchProgramError, 'Canvas course copy timed out after 3 minutes' if (Time.now - start_time) > 3.minutes # Fail safe so this doesn't poll forever.
+
+      sleep 5.seconds
+
+      response = CanvasAPI.client.get_copy_course_status(progress_url)
+    end
+    block.call
+  end
+
+  def self.create_canvas_sections!(base_course, section_names)
+    canvas_section_ids = []
+    section_names.each do |sname|
+      cs = CanvasAPI.client.create_lms_section(course_id: base_course.canvas_course_id, name: sname) 
+      canvas_section_ids << cs.id
+      ps = Section.create!(name: cs.name, base_course_id: base_course.id, canvas_section_id: cs.id)      
+    end
+    canvas_assignment_ids = CanvasAPI.client.get_assignments(base_course.canvas_course_id).map { |fa| fa['id'] }
+
+    # Add an AssignmentOverride date object to each assignment for each section so that 
+    # the Edit Assignment Dates page shows the sections.
+    CanvasAPI.client.create_assignment_overrides(base_course.canvas_course_id, canvas_assignment_ids, canvas_section_ids)
+  end
+
+private
+
+  def set_salesforce_vars(salesforce_program_id)
+    # TODO: Ideally, we would validate the Program info, like that there are Cohort Schedules and the "Coach Course Section Name" field is set,
+    # at the time we're launching it instead of as part of the background job (where this is run). That way we can give immediate feedback if they're trying
+    # to launch a program that has not been setup properly with the info we need to be able to launch it. This makes me wonder if we
+    # should be introducing back a local platform Program model that represents the mapping of all the program related stuff. E.g.
+    # we could use it to map LC to Accelerator courses. We could store the Cohort and Cohort Schedule names and use them to do the sync
+    # logic when there are new ones or changes. We could store the state of the program, whether it's active or not, so that we can filter out
+    # access to old content for returning LCs or Fellows who drop but come back for a future run. E.g current LCs shouldn't see attendance for
+    # for attendance assignments for previous semesters event though they are in those sections.
+    #
+    # If we did that, then we could create the Program active record before launching the job which does this validation in the main UI
+    # and then pass it to the job using a GlobalID parameter: https://edgeguides.rubyonrails.org/active_job_basics.html#globalid
+    # Task to track: https://app.asana.com/0/1174274412967132/1198877695027085
+
+    @salesforce_program = sf_client.find_program(id: salesforce_program_id)
+    if @salesforce_program.leadership_coach_course_section_name.blank?
+      raise LaunchProgramError, '''Section Name in LMS Coach Course'' Salesforce field not set on Program'
+    end
+
+    @lc_course_section_names = [ @salesforce_program.leadership_coach_course_section_name ]
+    @fellow_course_section_names = sf_client.get_cohort_schedule_section_names(salesforce_program_id)
+
+    if @fellow_course_section_names.blank?
+      raise LaunchProgramError, 'No Cohort Schedules found for this Salesforce Program. Make sure those are setup first.'
+    end
+  end
+
+  def sf_client
+    SalesforceAPI.client
+  end
+
 end
--- spec/services/launch_program_spec.rb
+++ spec/services/launch_program_spec.rb
@@ -3,35 +3,90 @@
 require 'rails_helper'
 
 RSpec.describe LaunchProgram do
+  let(:sf_program_id) { 'TestSalesforceProgramID' }
+  let(:fellow_course_name) { 'Test Fellow Course Name' }
+  let(:fellow_course_template) { create(:course_template, attributes_for(:course_template_with_resource)) }
+  let(:lc_course_template) { create(:course_template_with_canvas_id) }
+  let(:lc_course_name) { 'Test LC Course Name' }
+  let(:new_course_id) { 93487 }
+  let(:canvas_create_course) { build(:canvas_course, id: new_course_id) }
+  let(:canvas_copy_course) { build(:canvas_content_migration) }
+  let(:canvas_client) { double(CanvasAPI, create_course: canvas_create_course, copy_course: canvas_copy_course) }
+  let(:salesforce_program) { build(:salesforce_program_record) }
+  let(:fellow_section_names) { [] }
+  let(:sf_api_client) { SalesforceAPI.new }
+
+  before(:each) do
+    allow(LaunchProgram).to receive(:sleep).and_return(nil)
+    allow(CanvasAPI).to receive(:client).and_return(canvas_client)
+    allow(sf_api_client).to receive(:get_program_info).and_return(salesforce_program)
+    allow(sf_api_client).to receive(:get_cohort_schedule_section_names).and_return(fellow_section_names)
+    allow(SalesforceAPI).to receive(:client).and_return(sf_api_client)
+  end
+
+  subject(:launch_program) do
+    LaunchProgram.new(sf_program_id, fellow_course_template.id, fellow_course_name, lc_course_template.id, lc_course_name)
+  end
+
+  describe '#initialize' do
+
+    context 'with valid Program' do
+      let(:fellow_section_names) { [ 'Test CohortSchedule 1', 'Test CohortSchedule 2' ] }
+
+      it "doesn't raise error" do
+        expect { launch_program }.not_to raise_error
+      end
+    end
+    
+    context 'with invalid Program' do
+      it "raises error with bad Program ID" do
+        allow(sf_api_client).to receive(:get_program_info).and_raise RestClient::BadRequest
+        expect { launch_program }.to raise_error(RestClient::BadRequest)
+      end
+
+      it "raises an error when missing 'Section Name in LMS Coach Course' field" do
+        salesforce_program['Section_Name_in_LMS_Coach_Course__c'] = nil
+        expect { launch_program }.to raise_error(LaunchProgram::LaunchProgramError).with_message(/Section Name in LMS/)
+      end
+
+      it "raises an error when missing Cohort Schedules" do
+        expect { launch_program }.to raise_error(LaunchProgram::LaunchProgramError).with_message(/No Cohort Schedules/)
+      end
+    end
+  end
+
   describe '#run' do
-    let(:sf_program_id) { 'TestSalesforceProgramID' }
-    let(:fellow_course_name) { 'Test Fellow Course Name' }
-    let(:fellow_course_template) { create(:course_template, attributes_for(:course_template_with_resource)) }
-    let(:lc_course_template) { create(:course_template_with_canvas_id) }
-    let(:lc_course_name) { 'Test LC Course Name' }
-    let(:canvas_create_course) { build(:canvas_course) }
-    let(:canvas_copy_course) { build(:canvas_content_migration) }
-    let(:canvas_client) { double('CanvasAPI', create_course: canvas_create_course, copy_course: canvas_copy_course) }
+    let(:fellow_section_names) { [ 'Test CohortSchedule 1', 'Test CohortSchedule 2' ] }
+    let(:canvas_sections) { [] }
+    let(:assignment1) { build(:canvas_assignment, course_id: new_course_id) }
+    let(:assignment2) { build(:canvas_assignment, course_id: new_course_id) }
+    let(:assignments) { [assignment1, assignment2] }
+    let(:copy_progress_running) { build(:canvas_progress, workflow_state: 'running') }
+    let(:copy_progress_complete) { build(:canvas_progress, workflow_state: 'completed') }
 
     before(:each) do
-      allow(CanvasAPI).to receive(:client).and_return(canvas_client)
+      allow(canvas_client).to receive(:get_copy_course_status)
+        .and_return(copy_progress_running, copy_progress_complete, copy_progress_running, copy_progress_complete)
+      allow(canvas_client).to receive(:create_lms_section) { |course_id:, name:| 
+        new_section = build(:canvas_section)
+        canvas_sections << new_section
+        CanvasAPI::LMSSection.new(new_section['id'], name)
+      }
+      allow(canvas_client).to receive(:get_assignments).and_return(assignments) 
+      allow(canvas_client).to receive(:create_assignment_overrides)
     end
 
-    subject(:launch_program) do
-      LaunchProgram.new(sf_program_id, fellow_course_template.id, fellow_course_name, lc_course_template.id, lc_course_name).run
-    end
-
-    context "with canvas API success" do
+    context "with Canvas API success" do
       it "calls canvas API to copy both fellow and LC courses" do
         expect(canvas_client).to receive(:create_course).with(fellow_course_name).once
         expect(canvas_client).to receive(:create_course).with(lc_course_name).once
         expect(canvas_client).to receive(:copy_course).with(fellow_course_template.canvas_course_id, canvas_create_course['id']).once
         expect(canvas_client).to receive(:copy_course).with(lc_course_template.canvas_course_id, canvas_create_course['id']).once
-        launch_program
+        launch_program.run
       end
 
-      it "creates new platform courses" do
-        expect { launch_program }.to change(Course, :count).by(2)
+      it "creates new Courses in local database" do
+        expect { launch_program.run }.to change(Course, :count).by(2)
         fellow_course = Course.find_by(name: fellow_course_name)
         expect(fellow_course.canvas_course_id).to eq canvas_create_course['id']
         expect(fellow_course.course_resource_id).to eq fellow_course_template.course_resource_id
@@ -39,12 +94,31 @@ RSpec.describe LaunchProgram do
         expect(lc_course.canvas_course_id).to eq canvas_create_course['id']
         expect(lc_course.course_resource_id).to eq lc_course_template.course_resource_id
       end
+
+      it "waits for the Canvas course copy to finish before getting assignments" do
+        # twice for the Fellow course and twice for LC course
+        expect(canvas_client).to receive(:get_copy_course_status).with(canvas_copy_course['progress_url']).exactly(4).times
+        launch_program.run
+      end
+
+      it "creates the Canvas sections" do
+        expect(canvas_client).to receive(:create_lms_section).with(course_id: new_course_id, name: fellow_section_names[0]).once
+        expect(canvas_client).to receive(:create_lms_section).with(course_id: new_course_id, name: fellow_section_names[1]).once
+        expect(canvas_client).to receive(:create_lms_section).with(course_id: new_course_id, name: salesforce_program['Section_Name_in_LMS_Coach_Course__c']).once
+        launch_program.run
+      end
+
+      it "sets the AssignmentOverrides" do
+        launch_program.run
+        expect(canvas_client).to have_received(:create_assignment_overrides)
+          .with(new_course_id, [assignment1['id'], assignment2['id']], [canvas_sections[0]['id'], canvas_sections[1]['id']]).once
+      end
     end
 
-    context "with canvas API failure" do
+    context "with Canvas API failure" do
       it "raises an exception" do
         allow(canvas_client).to receive(:copy_course).and_raise(RestClient::NotFound)
-        expect { launch_program }.to raise_error(RestClient::NotFound)
+        expect { launch_program.run }.to raise_error(RestClient::NotFound)
       end
     end
   end
