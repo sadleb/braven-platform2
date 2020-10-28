7e29f7296df4427e1ce3d608b3738590d30761f6
Brian Sadler <sadler.brian@gmail.com>
2020-10-28 12:09:13 -0400

    Stop logging LtiLaunch cleanup job in Rails console (#404)
    When starting the Rails console, the CleanupLtiLaunchTableJob
    would run 30 seconds later and corrupt the terminal prompt right
    in the middle of typing commands. This just makes it run immediately in dev.
    You don't actually see anything logged b/c this seems to happen before the
    logger is all setup and hooked up to STDOUT, but if you drop a "puts"
    in the job you'll see that it's still running.
    
    Test Plan:
    Connect to the Rails console in dev and see that cursed "Running cleanup
    job" doesn't happen 30 seconds later while you're typing messing up the prompt.

--- config/initializers/delayed_jobs.rb
+++ config/initializers/delayed_jobs.rb
@@ -1,8 +1,12 @@
+# Add delayed_jobs that should be run on app startup in this file
 Rails.configuration.after_initialize do
+
   unless Rails.env.test? # These are annoying when running specs. We should test the jobs directly anyway and not as part of app startup
 
-    # Add delayed_jobs that should be run on app startup here:
-    CleanupLtiLaunchTableJob.set(wait: 30.seconds).perform_later || Rails.logger.error('Failed to enqueue CleanupLtiLaunchTableJob')
+    # Set delay to 0 in dev so that the Rails console doesn't log messages 30 seconds
+    # later when you're in the middle of typing commands.
+    lti_launch_cleanup_delay = (Rails.env.development? ? 0 : 30.seconds)
+    CleanupLtiLaunchTableJob.set(wait: lti_launch_cleanup_delay).perform_later || Rails.logger.error('Failed to enqueue CleanupLtiLaunchTableJob')
 
   end
 end
