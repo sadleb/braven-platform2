4a292fa7575e839bc05df2ea41ee8b98e5ced6ff
Ryan Shipp <1382374+rshipp@users.noreply.github.com>
2021-03-15 13:41:34 -0500

    Fix open redirect (#621)
    * Fix open redirect
    
    * Empty string is truthy :(

--- app/controllers/attendance_events_controller.rb
+++ app/controllers/attendance_events_controller.rb
@@ -23,9 +23,12 @@ class AttendanceEventsController < ApplicationController
       event_type: create_params[:event_type],
     )
 
+    redirect_path = create_params[:redirect_to_course].blank? ?
+       attendance_events_path : edit_course_path(create_params[:redirect_to_course])
+
     respond_to do |format|
       format.html { redirect_to(
-        create_params[:redirect_to] || attendance_events_path,
+        redirect_path,
         notice: 'Attendance event was successfully created.'
       ) }
       format.json { head :no_content }
@@ -54,7 +57,7 @@ private
     params.require(:attendance_event).permit(
       :title,
       :event_type,
-      :redirect_to,
+      :redirect_to_course,
     )
   end
 end
--- spec/controllers/attendance_events_controller_spec.rb
+++ spec/controllers/attendance_events_controller_spec.rb
@@ -68,12 +68,12 @@ RSpec.describe AttendanceEventsController, type: :controller do
         let(:custom_path) { edit_course_path(course) }
         let(:valid_attributes) { {
           title: 'Attendance Event Title',
-          redirect_to: custom_path,
+          redirect_to_course: course.id,
         } }
 
-        it 'redirects to the specified path' do
+        it 'redirects to the specified course edit page' do
           subject
-          expect(response).to redirect_to(custom_path)
+          expect(response).to redirect_to(edit_course_path(course.id))
         end
       end
     end
