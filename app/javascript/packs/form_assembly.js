bd7da6a9234a5e7d28f337e49d817b1d785d3cc5
Brian Sadler <sadler.brian@gmail.com>
2021-03-29 11:00:33 -0400

    Customize the Form Assembly e-Signature message about verifying email. (#647)
    * Customize the Form Assembly e-Signature message about verifying email.
    
    The built-in behavior for Form Assembly e-Signature forms is that on the
    final page where you sign, it shows a warning message saying:
    
    Incomplete Response
    Please click the link in the verification email to complete your signature.
    
    However, you don't get the email until after you click the button to submit.
    This is confusing and we have no way to control this on the Form Assembly
    side of things. So this commit uses JS to change the language to be more clear.
    
    * This happens on page load. No need to explicitly send a beacon.
    
    * Try and stick with our Honeycomb field naming conventions.
    
    Note: I'm going to followup with a separate PR to make the "controller"
    and "action" (aka the filename and function) that we construct the
    HoneycombSpan with more normalized when things make it to Honeycomb.
    
    E.g. things like the following
    
    name = js.page.load
    js.app.controller = form_assembly
    js.app.action = changeESignatureMessage
    js.app.name = js.form_assembly.changeESignatureMessage
    js.app.form_assembly.esignature_message_found = true
    
    * Add basic Jest test. Fixed bug it found.
    
    * Add test for running on page with no e-Sig warning message
    
    * Merge with: https://github.com/bebraven/platform/pull/649
    
    * snake_case to CamelCase in JS

---
+++ app/javascript/packs/form_assembly.js
@@ -0,0 +1,47 @@
+// This is responsible for any customization we have to do to the Form Assembly
+// using Javascript b/c there is no other way to do it. Try to avoid doing this!
+//
+// Run this script by adding
+//  <%= javascript_pack_tag 'form_assembly', 'data-turbolinks-track': 'reload' %>
+// to the head of your layout.
+
+import { HoneycombAddToSpan } from './honeycomb'
+
+export async function main() {
+
+  // The built-in behavior for Form Assembly e-Signature forms is that on the final
+  // page where you sign, it shows a warning message saying:
+  //
+  // Incomplete Response
+  // Please click the link in the verification email to complete your signature.
+  //
+  // However, you don't get the email until after you click the button to submit.
+  // This is confusing. The JS below changes the message to be more clear.
+  async function changeESignatureMessage() {
+    const honeySpan = new HoneycombAddToSpan('form_assembly', 'changeESignatureMessage');
+    try {
+      let esigWarning = document.querySelector('fieldset#tfa_esignature-email-warning');
+      if (esigWarning) {
+        honeySpan.addField('esignature_message_found', 'true');
+
+        let esigWarningLegend = esigWarning.querySelector('legend.errMsg');
+        esigWarningLegend.innerHTML = '<i class="fa fa-envelope" aria-hidden="true"></i> Check your email after submitting';
+        let esigWarningMsg = esigWarning.querySelector('div.errMsg');
+        esigWarningMsg.innerText = 'Please click the link in the verification email to complete your signature ' +
+                                   'after submitting the signed response below.';
+
+        honeySpan.addField('esignature_message_changed', 'true');
+      } else {
+        honeySpan.addField('esignature_message_found', 'false');
+      }
+    } catch (err) {
+      const errorMsg = 'Failed to customize the e-Signature warning message to be more clear what should be done after submitting.';
+      honeySpan.addErrorDetails(error_msg, err);
+    }
+  }
+
+  changeESignatureMessage();
+
+} // END: main
+
+window.onload = main;
---
+++ spec/javascript/packs/form_assembly.test.js
@@ -0,0 +1,66 @@
+import { HoneycombAddToSpan } from 'packs/honeycomb'
+jest.mock('packs/honeycomb');
+
+beforeEach(() => {
+  // Clear all instances and calls to constructor and all methods:
+  HoneycombAddToSpan.mockClear();
+});
+
+function setupHtml(esigwarningHtml = ''){
+  document.body.innerHTML = `
+    <form action="https://form.assembly.url/responses/processor" method="POST">
+      <h3>Your Signature</h3>
+      <fieldset id="tfa_esignature" class="section">
+        <legend id="tfa_esignature-L">Please sign here</legend>
+        <div class="wFormsSignature">
+          here is where the signature is with a bunch of inputs
+        </div>
+      </fieldset>
+      ${esigwarningHtml}
+      <div class="reviewActions actions">
+        <input type="submit" class="primaryAction slds-button slds-button--brand full-width" value="Submit Signed Response">
+      </div>
+    </form>`
+}
+
+test('changes the message when found', async () => {
+  let validESignatureWarningHtml = `
+    <style>
+    #tfa_esignature-email-warning {
+        padding: 16px 16px 20px 16px;
+    }
+  </style>
+  <fieldset id="tfa_esignature-email-warning" class="section errFld">
+      <legend id="tfa_esignature-email-warning-L" class="errMsg"><i class="fa fa-envelope" aria-hidden="true"></i> Incomplete Response</legend>
+      <div class="errMsg">
+          Please click the link in the verification email to complete your signature.    </div>
+  </fieldset>`
+
+  setupHtml(validESignatureWarningHtml);
+
+  const formAssembly = require('packs/form_assembly');
+  await formAssembly.main();
+
+  expect(document.querySelector('#tfa_esignature-email-warning legend.errMsg').innerHTML)
+    .toBe('<i class="fa fa-envelope" aria-hidden="true"></i> Check your email after submitting');
+
+  expect(document.querySelector('#tfa_esignature-email-warning div.errMsg').innerText)
+    .toBe('Please click the link in the verification email to complete your signature after submitting the signed response below.');
+});
+
+
+test('NOOP when not found', async () => {
+  let nonESignatureWarningHtml = '<div id="tfa_1-E" class="errMsg" tabindex="-1"><span>This field is required.</span></div>';
+  setupHtml(nonESignatureWarningHtml);
+
+  const formAssembly = require('packs/form_assembly');
+  await formAssembly.main();
+
+  expect(document.querySelector('#tfa_1-E').innerHTML)
+    .toBe('<span>This field is required.</span>');
+
+  expect(HoneycombAddToSpan).toHaveBeenCalled();
+  const mockHoneycombSpan = HoneycombAddToSpan.mock.instances[0];
+  const mockAddErrorDetails = mockHoneycombSpan.addErrorDetails;
+  expect(mockAddErrorDetails).not.toHaveBeenCalled();
+});
