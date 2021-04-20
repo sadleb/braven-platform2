131f8988449d85262e47a0f8389833524ca1f8bf
Brian Sadler <sadler.brian@gmail.com>
2021-04-20 15:29:02 -0400

    Change API access tokens from 16 to 255 characters. (#679)
    16 is not secure, esp for a key we don't rotate frequently (or at all)

--- app/models/access_token.rb
+++ app/models/access_token.rb
@@ -1,26 +1,20 @@
-require 'digest/md5'
-
 class AccessToken < ApplicationRecord
-  VALID_KEY = /\A[0-9a-f]{16}\z/i
-  
+
+  attr_readonly :key
+
   validates :name, presence: true, uniqueness: {case_sensitive: false}
-  validates :key, presence: true, uniqueness: {case_sensitive: false}, format: {with: VALID_KEY}
-  
-  before_validation :generate_key, if: :key_invalid?
-  
+  validates :key, presence: true, uniqueness: {case_sensitive: true}, length: {is: 255}
+
+  after_initialize :set_key
+
   class << self
     def generate_key
-      Digest::MD5.hexdigest([Time.now, rand].join(','))[0,16]
+      "#{Base64.urlsafe_encode64(SecureRandom.uuid)}.#{SecureRandom.urlsafe_base64(154)}"
     end
   end
-  
-  private
-  
-  def generate_key
-    self.key = self.class.generate_key
-  end
-  
-  def key_invalid?
-    key !~ VALID_KEY
+
+private
+  def set_key
+    self.key ||= self.class.generate_key
   end
 end
--- spec/models/access_token_spec.rb
+++ spec/models/access_token_spec.rb
@@ -4,73 +4,60 @@ RSpec.describe AccessToken, type: :model do
   ############
   # Validation
   ############
-  
+
   describe 'validation' do
-    before do
-      # disable auto-key generation to test validation
-      allow_any_instance_of(AccessToken).to receive(:generate_key).and_return(nil)
-    end
-  
     it { should validate_presence_of :name }
     it { should validate_presence_of :key }
-  
+
     describe 'uniqueness' do
       before { create :access_token }
-    
+
       it { should validate_uniqueness_of(:name).case_insensitive }
-      it { should validate_uniqueness_of(:key).case_insensitive }
+      it { should validate_uniqueness_of(:key) }
     end
-  
-    describe '16-digit hexadecimal key format' do
-      it { should allow_value('0000000000000001').for(:key) }
-      it { should allow_value('0123456789abcdef').for(:key) }
-    
+
+    describe '255-char Base64 format key' do
+      it { should allow_value('N2M2ODZiYzgtMTQ5MC00YTZhLThlOTQtYjhhZjJjNTZjODc0.4bgJEIOOI6n2ubk9dlvIYkcMjVln-7u0OPgWgIj7osAGBfxAs67AlWEy2zAb3mM2SBW0lM-U6Dz-4zbhQK-TKt4RR4Tqeqt7dfHpPzrT-mV-1kypNOdtNgM3FHOC70-v-3zqxW088b6Uf36Ugt6hsfR2XaHyCeKH0Bt4iEKQFKdaMUuB6o5cqDY6myY-5HAPsWk4k6qaxxPvGg').for(:key) }
+
+      it { should_not allow_value(nil).for(:key)}
       it { should_not allow_value('1').for(:key)}
-      it { should_not allow_value('000000000000000x').for(:key)}
+      it { should_not allow_value('N2M2ODZiYzgtMTQ5MC00YTZhLThlOTQtYjhhZjJjNTZjODc0.4bgJEIOOI6n2ubk9dlvIYkcMjVln-7u0OPgWgIj7osAGBfxAs67AlWEy2zAb3mM2SBW0lM-U6Dz-4zbhQK-TKt4RR4Tqeqt7dfHpPzrT-mV-1kypNOdtNgM3FHOC70-v-3zqxW088b6Uf36Ugt6hsfR2XaHyCeKH0Bt4iEKQFKdaMUuB6o5cqDY6myY-5HAPsWk4k6qaxxPvGgEXTRACHARSHERE').for(:key)}
     end
   end
-  
+
   ###########
   # Callbacks
   ###########
 
   describe 'generating key on create' do
-    let(:access_token) { AccessToken.create name: 'Name', key: key }
-    
-    subject { access_token.key }
-    
-    describe 'when valid key is provided' do
-      let(:key) { '0000111122223333' }
-      it { should eq(key) }
+    let(:key) { 'N2M2ODZiYzgtMTQ5MC00YTZhLThlOTQtYjhhZjJjNTZjODc0.4bgJEIOOI6n2ubk9dlvIYkcMjVln-7u0OPgWgIj7osAGBfxAs67AlWEy2zAb3mM2SBW0lM-U6Dz-4zbhQK-TKt4RR4Tqeqt7dfHpPzrT-mV-1kypNOdtNgM3FHOC70-v-3zqxW088b6Uf36Ugt6hsfR2XaHyCeKH0Bt4iEKQFKdaMUuB6o5cqDY6myY-5HAPsWk4k6qaxxPvGg' }
+    let(:access_token) { AccessToken.create name: 'Name' }
+
+    before(:each) do
+      allow(AccessToken).to receive(:generate_key).and_return(key)
     end
-    
-    describe 'when invalid key provided' do
-      let(:key) { '1' }
-      it { should_not eq(key) }
-      it { should match(AccessToken::VALID_KEY) }
+
+    it 'generates a valid key' do
+      access_token
+      expect(access_token.key).to eq(key)
     end
-    
-    describe 'when key is not provided' do
-      let(:key) { nil }
-      it { should match(AccessToken::VALID_KEY) }
-    end
+
   end
-  
+
   ###############
   # Class methods
   ###############
 
   describe 'generate_key' do
     subject { AccessToken.generate_key }
-    
-    it { should match(AccessToken::VALID_KEY) }
-    
-    it "generates a new key each time" do
-      attempts = 100
+
+    # This is just making sure there isn't a really dumb bug that returns the same key each time.
+    it "generates new keys each time" do
+      attempts = 10
       keys = []
-      
+
       attempts.times{ keys << AccessToken.generate_key }
-      
+
       expect(keys.uniq.size).to eq(attempts)
     end
   end
