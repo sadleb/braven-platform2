2c0bf53e2ff3543a1f7385c8c8f543ca21eb0f56
Brian Sadler <sadler.brian@gmail.com>
2021-05-10 12:04:43 -0400

    Skip creating assignment overrides when launching a Dummy LC Playbook. (#712)

--- app/services/initialize_new_course.rb
+++ app/services/initialize_new_course.rb
@@ -35,7 +35,9 @@ class InitializeNewCourse
 
       # Add an AssignmentOverride date object to each assignment for each section so that
       # the Edit Assignment Dates page shows the sections.
-      CanvasAPI.client.create_assignment_override_placeholders(@new_course.canvas_course_id, canvas_assignment_ids, canvas_section_ids)
+      if canvas_assignment_ids.present?
+        CanvasAPI.client.create_assignment_override_placeholders(@new_course.canvas_course_id, canvas_assignment_ids, canvas_section_ids)
+      end
     end
   end
 
