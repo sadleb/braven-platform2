ab3891f549aaf94265bb1177307f08761de62f3c
Brian Sadler <sadler.brian@gmail.com>
2021-07-28 16:21:16 -0400

    Remove TODO about jobs not handling exceptions properly. (#787)
    Task: https://app.asana.com/0/1174274412967132/1200659281191966
    
    Turns out that the GenerateZoomLinksJob was the last one that used
    the pattern from the old platform code that wasn't sending Sentry errors.
    The jobs all use the correct pattern now.

--- app/jobs/generate_zoom_links_job.rb
+++ app/jobs/generate_zoom_links_job.rb
@@ -24,17 +24,4 @@ class GenerateZoomLinksJob < ApplicationJob
       raise
     end
   end
-
-#  # TODO: get rid of all the rescue_from() calls in ALL our jobs. Either something changed
-#  # or it never worked for Sentry to automatically capture these when raised from here.
-#  # I think it's b/c the point of the rescue_from() handler is to handle the error
-#  # so that it's not considered a failed job. Just change them all to have a rescue in
-#  # the perform method that sends the mail and then re-raises. See above for the pattern to follow
-#
-#  https://app.asana.com/0/1174274412967132/1200659281191966
-#
-#  rescue_from(StandardError) do |exception|
-#    GenerateZoomLinksMailer.with(email: arguments.second).failure_email.deliver_now
-#    raise
-#  end
 end
