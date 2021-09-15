b344a73c36ecd700d42fce6efb9f46291f12c19f
Brian Sadler <sadler.brian@gmail.com>
2021-09-15 10:45:27 -0400

    Set all Module submissions to be on Jan 1, 1970 (#840)
    * Set all Module submissions to be on Jan 1, 1970
    
    Task: https://app.asana.com/0/1174274412967132/1200954533308126
    
    The Canvas UI will show "Late" in red if the submitted_at time is after
    the due date. We create a single placeholder submission the first time
    a Fellow opens the Module and use that to post auto-grades too. The
    "Submitted At" time has no meaning in terms of their grade since it's
    the timestamp of when they opened it. If they open it for the first time
    after the due date, it shows "Late", otherwise is always shows on-time.
    This can be confusing. With this change, "Late" will never be shown
    and the non-sensical date of Jan 1, 1970 should make support easier
    if Fellows needs help understanding their grade and why they didn't
    receive on-time credit. It'll be easy to say "ignore that, it doesn't
    mean anything" - the on-time credit is given when you reach 100% completion
    before the due date, which is shown on the grades page.
    
    Note: the proper fix for this would be to send a new submission to Canvas
    everytime they do more work. You can't update just the submited_at time.
    That would require a major refactor and may put unecessary load on the server
    in terms of API calls and database queries in addition to handling all the
    tricky logic to be able to send the proper grade everytime (for example,
    we need the submission to determine if the grade was manually overridde
    which we'd have to make an API call to get and we'd still have to handle
    0'ing out grades when the due date passes if they've never opened it as
    well as handling extensions as well as clicking around the Module which can
    send new progression statements to lrs_xapi_mock). This solution seemed to
    be the right balance of acheiving the intended behavior in a simple way.
    
    * Fix specs and switch to subsecond precision for timestamps.

--- lib/lti_score.rb
+++ lib/lti_score.rb
@@ -2,8 +2,11 @@
 
 # Represents an LTI score message sent to the API. See:
 # https://canvas.instructure.com/doc/api/score.html
+#
+# Note: the docs for this say to use sub-second precision which differs from
+# the CanvasAPI docs. That's why use use iso8601(3) instead of plain iso8601
 class LtiScore
-  
+
   LTI_SCORE_SUBMISSION_URL_KEY = "https://canvas.instructure.com/lti/submission"
 
   module ActivityProgress
@@ -11,9 +14,9 @@ class LtiScore
     STARTED='Started'
     IN_PROGRESS='InProgress'
     SUBMITTED='Submitted'
-    COMPLETED='Completed' 
+    COMPLETED='Completed'
   end
- 
+
   module GradingProgress
     NOT_READY = 'NotReady'
     FAILED = 'Failed'
@@ -25,10 +28,28 @@ class LtiScore
   include ActivityProgress
   include GradingProgress
 
+  # This is weird, but here's what's going on... Canvas will show a big red "Late"
+  # indication in the UI depending on the submitted_at time. For Modules, we're just creating
+  # a single submission the first time they open the Module. That "Late" indication is
+  # meaningless and only shows up if they open the module for the first time after the due date.
+  # So we're setting it to Jan 1st, 1970 (the 1 day offset from Epoch 0 is so that anything
+  # in the US will be on the 1st instead of Dec 31st 1969 in their local timezone). This way,
+  # nothing ever shows as "Late" in the Canvas UI and instead they can see how their grade was
+  # calculated by going to the Rise360ModuleGradesController#show endpoint. The submitted_at
+  # date is clearly non-sensical so it'll be easier to support questions like
+  # "why didn't I get on-time credit when it says Submitted At blah?"
+  #
+  # To fix this properly, we would need to refactor grading to always create a new submission
+  # when they do new work in the Module. It's WAY too much work to do that refactoring.
+  # We also talked about using hacky Javascript to hide the "Late" stuff in the UI but it
+  # would be nearly impossible to do for only Modules and not Projects as well which would
+  # disable our ability to use the Canvas out-of-the box Late Policy Status stuff in the future.
+  NON_SENSICAL_SUBMITTED_AT = Time.at(86400).utc.iso8601(3)
+
   # Generates an LtiScore that represents a new Project submission where
   # a Teaching Assitant or other staff must grade the project.
   def self.new_project_submission(canvas_user_id, submission_url)
-    new_pending_manual_submission(canvas_user_id, submission_url)
+    generate(canvas_user_id, SUBMITTED, PENDING_MANUAL, basic_lti_launch_submission(submission_url))
   end
 
   # Generates an LtiScore that represents a new Module submission where
@@ -41,19 +62,14 @@ class LtiScore
     # on the Canvas side next time you try to retrieve the score. I think it's bc
     # of the comments in API docs about only PENDING_MANUAL and FULLY_GRADED causing
     # an associated submission to be created for the score line_item.
-    new_pending_manual_submission(canvas_user_id, submission_url)
+    generate(canvas_user_id, IN_PROGRESS, PENDING_MANUAL, basic_lti_launch_submission(submission_url, NON_SENSICAL_SUBMITTED_AT))
   end
 
   # Generates an LtiScore that represents a new submission where no manual
   # grading is needed. The score is for full credit.
   def self.new_full_credit_submission(canvas_user_id, submission_url)
-    submission_data = {
-      new_submission: true,
-      submission_type: 'basic_lti_launch',
-      submission_data: submission_url,
-    }
     # The number 100 is arbitrary. This is just full credit: 100/100.
-    generate(canvas_user_id, SUBMITTED, FULLY_GRADED, submission_data, 100, 100)
+    generate(canvas_user_id, SUBMITTED, FULLY_GRADED, basic_lti_launch_submission(submission_url), 100, 100)
   end
 
 private
@@ -71,26 +87,26 @@ private
   #                   Failed, and Pending will cause the scoreGiven to be ignored. FullyGraded
   #                   values will require no action. Possible values are NotReady, Failed, Pending,
   #                   PendingManual, FullyGraded
-  def self.generate(user_id, activity_progress = COMPLETED, grading_progress = FULLY_GRADED, submission = nil, score_given = nil, score_maximum = nil, comment = nil)
+  def self.generate(user_id, activity_progress = COMPLETED, grading_progress = FULLY_GRADED, submission = nil, score_given = nil, score_maximum = nil)
     msg = {
       :userId => user_id.to_s,
-      :timestamp => DateTime.now,
+      :timestamp => Time.now.utc.iso8601(3),
       :activityProgress => activity_progress,
       :gradingProgress => grading_progress,
     }
     msg[LTI_SCORE_SUBMISSION_URL_KEY] = submission if submission
     msg[:scoreGiven] = score_given if score_given
     msg[:scoreMaximum] = score_maximum if score_maximum
-    msg[:comment] = comment if comment
     msg.to_json
   end
 
-  def self.new_pending_manual_submission(canvas_user_id, submission_url)
-    submission_data = {
-      :new_submission => true,
-      :submission_type => 'basic_lti_launch',
-      :submission_data => submission_url
+  def self.basic_lti_launch_submission(submission_url, submitted_at = Time.now.utc.iso8601(3))
+    {
+      new_submission: true,
+      submission_type: 'basic_lti_launch',
+      submission_data: submission_url,
+      submitted_at: submitted_at,
     }
-    generate(canvas_user_id, SUBMITTED, PENDING_MANUAL, submission_data)
   end
+
 end
--- spec/lib/lti_score_spec.rb
+++ spec/lib/lti_score_spec.rb
@@ -8,17 +8,18 @@ RSpec.describe LtiScore do
       let(:user_id) { 1234 }
       let(:submission_url) { 'https://platformweb/the/url/to/view/submission' }
       it 'generates a new_submission message for the Canvas score API ' do
-        allow(DateTime).to receive(:now).and_return(DateTime.now)
+        allow(Time).to receive(:now).and_return(Time.now)
         expect(JSON.parse(LtiScore.new_project_submission(user_id, submission_url))).to eq(
           {
             'userId' => user_id.to_s,
-            'timestamp' => DateTime.now.iso8601(3), # 3 decimal precision of milliseconds
+            'timestamp' => Time.now.utc.iso8601(3), # 3 decimal precision of milliseconds
             'activityProgress' => LtiScore::ActivityProgress::SUBMITTED,
             'gradingProgress' => LtiScore::GradingProgress::PENDING_MANUAL,
             'https://canvas.instructure.com/lti/submission' => {
               'new_submission' => true,
               'submission_type' => 'basic_lti_launch',
-              'submission_data' => submission_url
+              'submission_data' => submission_url,
+              'submitted_at' => Time.now.utc.iso8601(3)
             }
           }
         )
@@ -31,11 +32,11 @@ RSpec.describe LtiScore do
       let(:user_id) { 1235 }
       let(:submission_url) { 'https://platformweb/the/url/to/view/submission' }
       it 'generates a new_submission message for the Canvas score API ' do
-        allow(DateTime).to receive(:now).and_return(DateTime.now)
+        allow(Time).to receive(:now).and_return(Time.now)
         expect(JSON.parse(LtiScore.new_full_credit_submission(user_id, submission_url))).to eq(
           {
             'userId' => user_id.to_s,
-            'timestamp' => DateTime.now.iso8601(3), # 3 decimal precision of milliseconds
+            'timestamp' => Time.now.utc.iso8601(3), # 3 decimal precision of milliseconds
             'activityProgress' => LtiScore::ActivityProgress::SUBMITTED,
             'gradingProgress' => LtiScore::GradingProgress::FULLY_GRADED,
             'scoreGiven' => 100,
@@ -43,7 +44,32 @@ RSpec.describe LtiScore do
             'https://canvas.instructure.com/lti/submission' => {
               'new_submission' => true,
               'submission_type' => 'basic_lti_launch',
-              'submission_data' => submission_url
+              'submission_data' => submission_url,
+              'submitted_at' => Time.now.utc.iso8601(3)
+            }
+          }
+        )
+      end
+    end
+  end
+
+  describe '#new_module submission' do
+    context 'when it''s the first submission' do
+      let(:user_id) { 1236 }
+      let(:submission_url) { 'https://platformweb/the/url/to/view/submission' }
+      it 'generates a new_submission message for the Canvas score API ' do
+        allow(Time).to receive(:now).and_return(Time.now)
+        expect(JSON.parse(LtiScore.new_module_submission(user_id, submission_url))).to eq(
+          {
+            'userId' => user_id.to_s,
+            'timestamp' => Time.now.utc.iso8601(3), # 3 decimal precision of milliseconds
+            'activityProgress' => LtiScore::ActivityProgress::IN_PROGRESS,
+            'gradingProgress' => LtiScore::GradingProgress::PENDING_MANUAL,
+            'https://canvas.instructure.com/lti/submission' => {
+              'new_submission' => true,
+              'submission_type' => 'basic_lti_launch',
+              'submission_data' => submission_url,
+              'submitted_at' => LtiScore::NON_SENSICAL_SUBMITTED_AT
             }
           }
         )
