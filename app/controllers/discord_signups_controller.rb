9d7e8eece4546c38c7876fef453d5195d9f7a0c2
Elena Baurkot <elenabaurkot@gmail.com>
2021-10-21 14:24:28 -0400

    Make Discord server Id dynamic  (#881)
    * attempting to get discord server dynamically
    
    * server id in struct
    
    * cleanup
    
    * dynamic server id working
    
    * Added exception if discord server id not found
    
    * update raising error
    
    * review updates
    
    * shorten line
    
    * added frozen_string_liter: true

--- app/controllers/discord_signups_controller.rb
+++ app/controllers/discord_signups_controller.rb
@@ -1,7 +1,11 @@
+# frozen_string_literal: true
 require "discordrb"
+require "salesforce_api"
+
 #TODO:implement everything --> this is demo
 class DiscordSignupsController < ApplicationController
   layout 'lti_canvas'
+  class DiscordServerIdError  < StandardError; end
 
   def launch
     authorize :discord_signups
@@ -10,8 +14,21 @@ class DiscordSignupsController < ApplicationController
       response = Discordrb::API::User.profile("Bearer #{current_user.discord_token}")
       @discord_user = JSON.parse(response.body)
 
-      if @discord_user["email"] && @discord_user["verified"]
-        Discordrb::API::Server.add_member("Bot #{Rails.application.secrets.bot_token}", "722098701878689933", @discord_user["id"], current_user.discord_token)
+      if @discord_user['email'] && @discord_user['verified']
+        # TODO: use the current course in addition to the current_user to ensure that we get the
+        # correct Participant. See: https://app.asana.com/0/1201131148207877/1201217979889309
+        participant = SalesforceAPI.client.find_participant(contact_id: current_user.salesforce_id)
+        discord_server_id = participant.discord_server_id
+
+        raise DiscordServerIdError, "No Discord Server Id found for Participant.Id = #{participant.id}" if discord_server_id.nil?
+
+        # Add user to their Discord Server
+        Discordrb::API::Server.add_member(
+          "Bot #{Rails.application.secrets.bot_token}",
+          discord_server_id,
+          @discord_user['id'],
+          current_user.discord_token
+        )
       end
     end
   end
@@ -19,9 +36,22 @@ class DiscordSignupsController < ApplicationController
   def oauth
     authorize :discord_signups
 
-    response = Discordrb::API.request(:oauth2_token, nil, :post, 'https://discord.com/api/v8/oauth2/token', {client_id: Rails.application.secrets.discord_client_id, client_secret: Rails.application.secrets.discord_client_secret, grant_type: 'authorization_code', code: params[:code], redirect_uri: Rails.application.secrets.discord_redirect_uri}, content_type: 'application/x-www-form-urlencoded')
+    response = Discordrb::API.request(
+      :oauth2_token,
+      nil,
+      :post,
+      'https://discord.com/api/v8/oauth2/token',
+      {
+        client_id: Rails.application.secrets.discord_client_id,
+        client_secret: Rails.application.secrets.discord_client_secret,
+        grant_type: 'authorization_code',
+        code: params[:code],
+        redirect_uri: Rails.application.secrets.discord_redirect_uri
+      },
+      content_type: 'application/x-www-form-urlencoded'
+    )
 
-    current_user.update!(discord_token: JSON.parse(response.body)["access_token"])
+    current_user.update!(discord_token: JSON.parse(response.body)['access_token'])
 
     redirect_to launch_discord_signups_url
   end
