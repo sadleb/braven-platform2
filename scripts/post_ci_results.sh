e0dd200d791f14ff29ea4a1d38c9542b77d6fba8
Ryan Shipp <1382374+rshipp@users.noreply.github.com>
2022-02-18 14:45:20 -0600

    Re-enable coverage reporting (#996)

--- scripts/post_ci_results.sh
+++ scripts/post_ci_results.sh
@@ -2,6 +2,5 @@
 #wget -qO - https://coverage.codacy.com/get.sh | sh -s report -r coverage/coverage.xml
 
 # Bash
-# TODO: something broke. Disabling for now so we can push.
-# Task to fix: https://app.asana.com/0/1201131148207877/1201474657154240
-#bash <(curl -Ls https://coverage.codacy.com/get.sh) report -r coverage/coverage.xml
+# Don't fail the CI if this breaks.
+bash <(curl -Ls https://coverage.codacy.com/get.sh) report -r coverage/coverage.xml || true
